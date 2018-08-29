e04a59efc90a6d668cd754bf51302a5fbd9bb233
Jordan Lewis <jordanlewis@Jordans-MacBook-Pro.local>
2018-08-29 15:06:26 -0700

    Initial Commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,62 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+import { CommonModule } from '@angular/common';
+
+import { routing } from './app.routes';
+
+import { AppService } from './app.service';
+import { ExportService } from './export/export.service';
+
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenubarComponent } from './menubar/menubar.component';
+import { ClearComponent } from './clear/clear.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotFoundComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenubarComponent,
+    ClearComponent,
+    SkillsComponent,
+    EducationComponent,
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routing,
+    Ng2SmartTableModule,
+    BrowserAnimationsModule,
+    NgbModule.forRoot(),
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      progressBar: true,
+      closeButton: true
+    }),
+  ],
+  providers: [
+    AppService,
+    ExportService,
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: []
+})
+export class AppModule { }
